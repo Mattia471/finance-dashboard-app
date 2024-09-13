28f465e678a8edc0e76d3791e4728aa00934c7eb
Mattia Cucuzza <mcucuzza@netgroupemea.com>
2024-09-13 14:52:26 +0200

    feat: implemented Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import {Outlet} from "react-router-dom";
+import Navbar from "./components/Navbar.tsx";
 
 function App() {
     return (
         <React.Fragment>
-            <Outlet/>
+            <div className="app-container">
+                <Navbar/>
+                <div className="content-container">
+                    <Outlet/>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
