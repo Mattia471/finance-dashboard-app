f514d900d0b5f46df572d6920581705b11f3ba0f
Mattia Cucuzza <mcucuzza@netgroupemea.com>
2024-09-17 16:19:42 +0200

    fix: directory and files

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,10 +13,7 @@ app.use(express.json());
 app.use('/api/transactions', transactionRoutes);
 
 mongoose
-    .connect(process.env.MONGO_URI || '', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI || '', {})
     .then(() => console.log('MongoDB connected'))
     .catch((err) => console.error(err));
 
